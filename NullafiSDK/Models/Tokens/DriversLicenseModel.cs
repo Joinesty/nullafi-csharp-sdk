04b4287a7adcc75facaa11943f4ef6f187d35632
Jose Souza <jcarlosjunior@live.com>
2019-04-19 15:35:42 -0300

    adding more models

---
+++ NullafiSDK/Models/Tokens/DriversLicenseModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullafiSDK.Models.Tokens
+{
+    public class DriversLicenseModel
+    {
+       public string Id { get; set; }
+        [JsonProperty(PropertyName = "driverslicense")]
+        public string DriversLicense { get; set; }
+        [JsonProperty(PropertyName = "driverslicenseToken")]
+        public string DriversLicenseToken { get; set; }
+        public string Iv { get; set; }
+        public string AuthTag { get; set; }
+        public string Tags { get; set; }
+    }
+}
--- NullafiSDK/Models/Tokens/RandomModel.cs
+++ NullafiSDK/Models/Tokens/RandomModel.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NullafiSDK.Models
+namespace NullafiSDK.Models.Tokens
 {
-    public class AddressModel
+    public class RandomModel
     {
         public string Id { get; set; }
-        public string Address { get; set; }
-        public string AddressToken { get; set; }
+        public string Data { get; set; }
+        public string Token { get; set; }
         public string Iv { get; set; }
         public string AuthTag { get; set; }
         public string Tags { get; set; }
